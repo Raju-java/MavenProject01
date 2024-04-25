c24c64d298a708c7bc74cc6d4cf40d6fe78d2e48
Raju-java <raju16.adn@gmail.com>
2024-04-25 22:20:43 +0530

    modified 1

--- src/main/java/com/nit/service/App.java
+++ src/main/java/com/nit/service/App.java
@@ -16,11 +16,11 @@ public class App
     public static void main( String[] args )
     {
     	App app = new App();
-<<<<<<< HEAD
-        app.multiple(10,20);
-=======
+
+       
+
         app.multiple(10,20);	
->>>>>>> 32c5a8d6de635b2268bff89f14beb996f0bbdd36
+
 	System.out.println("Multiple of two numbers :"+app.multiple(10,20));
     }
 }
