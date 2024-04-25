61f2fc94698423c57be4390e2d6389da4f5a15ea
Raju-java <raju16.adn@gmail.com>
2024-04-25 19:29:36 +0530

    dev2 add multiple fuction to the main class

--- src/main/java/com/nit/service/App.java
+++ src/main/java/com/nit/service/App.java
@@ -9,9 +9,14 @@ public class App
 	public int sum(int a, int b) {
 		return a+b;
 	}
+	public void multiple(int a int b)
+	{
+	System.out.println("Multiple of two numbers : " + a*b)
+	}
     public static void main( String[] args )
     {
     	App app = new App();
         System.out.println( "Sum of two numbers: "+app.sum(10, 20) );
+	System.out.println("Multiple of two numbers :"+app.multiple(10,20));
     }
 }
